1c817de2639b9600a0ebb4a3caec360c4b4c31e0
Walid Lamraoui <m4dcod3r@protonmail.com>
2026-09-01 14:19:32 +0100

    minor changes

--- components/home/hero/Hero.tsx
+++ components/home/hero/Hero.tsx
@@ -36,19 +36,6 @@ export const Hero = () => {
             </StandardButton>
           </Reveal>
         </div>
-        <motion.div
-          initial={{ y: -30, opacity: 0 }}
-          animate={{ y: 0, opacity: 1 }}
-          transition={{ duration: 0.5, delay: 0.4 }}>
-          <Image
-            className={styles.profile}
-            src={Profile}
-            priority
-            alt="Walid Lamraoui | Fullstack Developer"
-            width={250}
-            height={250}
-          />
-        </motion.div>
       </div>
       <DotGrid />
     </section>
